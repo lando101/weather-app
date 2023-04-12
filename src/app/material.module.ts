509571c2a2afaee717cd871994ed7d7a20e22d93
lando101 <landon.messmer2019@gmail.com>
2023-04-12 19:34:45 -0400

    added images

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,10 +6,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatGridListModule} from '@angular/material/grid-list';
-
+import {MAT_TOOLTIP_DEFAULT_OPTIONS, MatTooltipDefaultOptions, MatTooltipModule} from '@angular/material/tooltip';
+export const myCustomTooltipDefaults: MatTooltipDefaultOptions = {
+  showDelay: 700,
+  hideDelay: 200,
+  touchendHideDelay: 1000,
+};
 @NgModule({
-    imports: [MatToolbarModule, MatMenuModule, MatSlideToggleModule, BrowserAnimationsModule, MatIconModule, MatTabsModule, MatGridListModule],
-    exports: [MatToolbarModule, MatMenuModule, BrowserAnimationsModule, MatSlideToggleModule, MatIconModule, MatTabsModule, MatGridListModule]
+    imports: [MatToolbarModule, MatMenuModule, MatSlideToggleModule, BrowserAnimationsModule, MatIconModule, MatTabsModule, MatGridListModule, MatTooltipModule],
+    exports: [MatToolbarModule, MatMenuModule, BrowserAnimationsModule, MatSlideToggleModule, MatIconModule, MatTabsModule, MatGridListModule, MatTooltipModule],
+    providers: [{
+      provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: myCustomTooltipDefaults
+    }]
   })
 
 export class MaterialModule { }
